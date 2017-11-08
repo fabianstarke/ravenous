0214a85ca2bcfb8a6ba601e384d44f78c90fb90f
Fabian Starke <fabian@starke.fr>
2017-11-08 12:11:21 +0100

    feat(ravenous): create searchbar, business list components

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# ravenous
+Build a website called "Ravenous", a Yelp-like clone.
+
+In total, there will be four parts to this project:
+
+Creating Static Components
+Passing Information to Components
+Setting the State of Ravenous Components
+Interacting with the Yelp API
---
+++ ravenous/src/components/searchBar/searchBar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './searchBar.css';
+
+const sortByOption = {
+  'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+  renderSortByOption(){
+    return Object.keys(sortByOption).map(sortByOption => {
+      let sortByOptionValue = sortByOption[sortByOption]
+    return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+
+  render(){
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOption}</ul>
+        </div>
+        <div class="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div class="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
